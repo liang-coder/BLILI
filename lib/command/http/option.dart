7b7e3e00c16401cd5b6252c207c95693d1ff369f
XL <2674353188@qq.com>
2026-01-01 22:17:23 +0800

    2026.1.1

--- lib/command/http/option.dart
+++ lib/command/http/option.dart
@@ -4,9 +4,17 @@ import '../utils/device/id.dart';
 import '../utils/device/deviceinfo.dart';
 
 class htppoption {
+
   static final Options _options = Options()..headers = _headers();
+  static final String _useragent1 =
+      'Mozilla/5.0 BiliDroid/2.0.1 (bbcallen@gmail.com) os/android model/HPB-AN00 mobi_app/android_hd build/2001100 channel/bili innerVer/2001100 osVer/9 network/2';
+  static final String _useragent2 =
+      'Mozilla/5.0 BiliDroid/2.0.1 (bbcallen@gmail.com) os/android model/HPB-AN00 mobi_app/android_hd build/2001100 channel/bili innerVer/2001100 osVer/9 network/2';
 
   static Options get getoptiion => _options;
+  static String get useragent1 =>_useragent1;
+  static String get useragent2 =>_useragent2;
+
 
   static Map<String, dynamic> _headers() {
     return {
