2d9f37b15596828fdf81215836ed7523a392d63a
xl <116136627+liang-coder@users.noreply.github.com>
2026-02-04 20:29:41 +0800

    Delete test directory

--- test/BDeviceInfoTest.dart
+++
@@ -1,9 +0,0 @@
-import 'package:blili/command/utils/device/id.dart';
-
-void main() {
-  // 模拟 d() 方法返回的 JSON
-  String deviceJson = '{"imei":"","androidid":"a1b2c3d4e5f67890","oaid":""}';
-
-  final String encrypted = Id.deviceInfoid(device: deviceJson);
-  print('Encrypted deviceinfo: $encrypted');
-}
--- test/BuvidTest.dart
+++
@@ -1,5 +0,0 @@
-import 'package:blili/command/utils/device/id.dart';
-
-void main() {
-  Id.buvid();
-}
